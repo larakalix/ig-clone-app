071e8f914dff90c1a72bbdaa51bee4fe787545b5
Ivan Lara <uki@live.co.uk>
2022-04-24 14:49:12 -0600

    Fix photo width to full on post detail

--- src/components/story/StoryHeader.tsx
+++ src/components/story/StoryHeader.tsx
@@ -6,7 +6,7 @@ export const StoryHeader = ({ story }: { story: Story }) => {
 
     return (
         <div className="overflow-hidden rounded-2xl w-full md:max-w-full lg:max-w-[30vw] xl::max-w-[40vw]">
-            <img src={image} alt={title} />
+            <img src={image} alt={title} className="w-full" />
         </div>
     );
 };
