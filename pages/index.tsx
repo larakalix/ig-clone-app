d0b97983098200fabfd5d624576382bdc1c830bc
Ivan Lara <uki@live.co.uk>
2022-04-24 20:24:41 -0600

    Add OG meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,21 @@ const Home: NextPage = () => {
         <>
             <Head>
                 <title>Instagram Clone</title>
+
                 <meta
-                    name="description"
-                    content="I built this app just for personal practice reasons as part of my portfolio"
+                    property="og:url"
+                    content="https://ig-clone-app.vercel.app/"
                 />
-                <meta name="image" content="/images/preview.png" />
+                <meta property="og:title" content="Instagram Clone" />
+                <meta
+                    property="og:image"
+                    content="https://ig-clone-app.vercel.app/images/preview.png"
+                />
+                <meta
+                    property="og:description"
+                    content="I built this app just for personal practice reasons as part of my portfolio."
+                />
+
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
