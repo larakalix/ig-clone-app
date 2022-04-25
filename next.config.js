bc5f7e36a05301cc6493da33cf4e37f0e9fec796
Ivan Lara <uki@live.co.uk>
2022-04-25 10:01:33 -0600

    Fix env variables

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         NEXT_DATO_CMS_TOKEN: process.env.NEXT_DATO_CMS_TOKEN,
-        NEXT_TITLE: process.env.NEXT_TITLE,
+        NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
     },
     images: {
         domains: ["www.datocms-assets.com"],
